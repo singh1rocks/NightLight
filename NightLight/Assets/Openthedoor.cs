bed8aa23bbdeabfe45617820e074e9e6a79cb508
RongdaWei <attcou1234@gmail.com>
2019-01-26 11:48:50 -0700

    doorDestroy

---
+++ NightLight/Assets/Openthedoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Openthedoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                print("shhhh");
+                Destroy(gameObject);
+            }
+        }
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
