f8e1a232aea16aeedac136b98a7df603b016b64b
Papapopp <32002023+Papapopp@users.noreply.github.com>
2019-01-26 15:39:02 -0700

    scripts from test directory

---
+++ NightLight/Assets/Scripts/PlayerInteractor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractor : MonoBehaviour
+{
+    public float InteractDistance = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, InteractDistance);
+
+        foreach (RaycastHit hit in hits)
+        {
+            Iinteractable doScript = hit.transform.GetComponent<Iinteractable>();
+            //check if it is interactible, and the player is interacting with it.
+            if (doScript != null && Input.GetKeyDown(KeyCode.F))
+            {
+                //if player is not frozen
+                if ()
+            }
+        }
+    }
+}
