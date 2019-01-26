ebbc5c2eac58515b7c3b6b65c15534746fa90168
Schir <schirsa@gmail.com>
2019-01-25 23:07:25 -0700

    added the basic part of a camera rotator

---
+++ NightLight/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    float horizontalSpeed = 2.0f;
+    float verticalSpeed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(-v, 0f, 0f);
+        transform.Rotate(0f, h, 0f);
+    }
+}
