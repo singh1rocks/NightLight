df3473505df4db1c05b9d0c5187e67e93082b724
shak54172 <shak54172@gmail.com>
2019-01-27 01:29:53 -0700

    Final Push of the night :(

---
+++ NightLight/Assets/LidForce.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LidForce : MonoBehaviour
+{
+    public Rigidbody lidRB;
+    public float speed;
+    public bool touched = true;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (touched == true)
+        {
+            touched = false;
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(speed, speed * 2, speed - 3));
+        }
+    
+    }
+}
---
+++ NightLight/Assets/LidTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LidTrigger : MonoBehaviour
+{
+    public float speed;
+    public GameObject lid;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            lid.GetComponent<Rigidbody>().AddForce(new Vector3(speed, speed * 2, speed - 3));
+        }
+    }
+}
