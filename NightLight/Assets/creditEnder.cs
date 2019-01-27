9297cec756a0e067818ad41b26f0b56c2aa62ca1
shak54172 <shak54172@gmail.com>
2019-01-27 14:46:23 -0700

    Almost there

---
+++ NightLight/Assets/creditEnder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creditEnder : MonoBehaviour
+{
+    LevelLoader LL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LL = GameObject.Find("gameManager").GetComponent<LevelLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(TimeToGo());
+
+    }
+
+    private IEnumerator TimeToGo()
+    {
+        yield return new WaitForSeconds(7f);
+        LL.CompleteLevel();
+        LL.TransisitionScene();
+    }
+}
