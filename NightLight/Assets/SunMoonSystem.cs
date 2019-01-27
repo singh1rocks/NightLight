9aed4232d79044ffebf79fb621972b9e1a387a02
Papapopp <32002023+Papapopp@users.noreply.github.com>
2019-01-27 12:27:16 -0700

    rotating light

---
+++ NightLight/Assets/SunMoonSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMoonSystem : MonoBehaviour
+{
+    GameObject gameManager;
+    DayNightClock clock;
+    void Start()
+    {
+        gameManager = GameObject.Find("gameManager");
+        clock = gameManager.GetComponent<DayNightClock>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = new Vector3(clock.timeOfDay * (360/100), 0, 0);
+        Quaternion q = Quaternion.Euler(v);
+        gameObject.transform.rotation = q;
+    }
+}
