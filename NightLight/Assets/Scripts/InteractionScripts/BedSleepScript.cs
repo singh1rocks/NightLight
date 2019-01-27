d3a33ca354abc49a66b9acf3258ce3cc22f861a8
LoganDuffy702 <duffyhawks96@gmail.com>
2019-01-27 14:25:01 -0700

    seems fine

--- NightLight/Assets/Scripts/InteractionScripts/BedSleepScript.cs
+++ NightLight/Assets/Scripts/InteractionScripts/BedSleepScript.cs
@@ -6,12 +6,17 @@ public class BedSleepScript : MonoBehaviour, Iinteractable
 {
     Dictionary<string, bool> PlayerInventory;
     TextDisplay txtDisplay;
+    public GameObject sleeperCam;
+    public Camera mainCamera;
+    private Camera layCam;
+    LevelLoader ll;
 
     void Start()
     {
         txtDisplay = GameObject.FindGameObjectWithTag("TextDisplay").GetComponent<TextDisplay>();
         //PlayerInventory =  GameObject.FindGameObjectWithTag("GameManager").GetComponent<Inventory>().PlayerItems;
         PlayerInventory = GameObject.Find("gameManager").GetComponent<Inventory>().PlayerItems;
+        ll = GameObject.Find("gameManager").GetComponent<LevelLoader>();
     }
 
     // Update is called once per frame
@@ -21,7 +26,26 @@ public class BedSleepScript : MonoBehaviour, Iinteractable
     }
     public void doPlayerInteraction()
     {
-        LevelLoader ll = GameObject.Find("gameManager").GetComponent<LevelLoader>();
+        
+        //spawn the laying down object
+        if (sleeperCam != null)
+        {
+            var newCamera = Instantiate(sleeperCam);
+            newCamera.transform.position = new Vector3(gameObject.transform.position.x-1.7f, gameObject.transform.position.y + 1.5f, gameObject.transform.position.z);
+            //translate it if necessary
+            layCam = newCamera.GetComponentInChildren<Camera>();
+        }
+        //switch camera
+        mainCamera.enabled = false;
+        layCam.enabled = true;
+
+        //wait for it to finish
+        StartCoroutine(GoToSleep());
+        
+    }
+    public IEnumerator GoToSleep()
+    {
+        yield return new WaitForSeconds(5);
         ll.TransisitionScene();
     }
 }
