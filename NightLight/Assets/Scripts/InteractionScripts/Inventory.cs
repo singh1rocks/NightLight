faf3a24feaee2491d0292d4529db2183d52a3b0c
Byreave Luo <654297651@qq.com>
2019-01-26 17:17:55 -0700

    add door open

---
+++ NightLight/Assets/Scripts/InteractionScripts/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Dictionary<string, bool> PlayerItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerItems = new Dictionary<string, bool>();
+        PlayerItems.Add("Day1Key", false);
+        PlayerItems.Add("Day1MatchBox", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
