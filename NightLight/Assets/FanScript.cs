f1305d21e3eb6fd495d3ffe6e4552449b5c050fe
LoganDuffy702 <32749000+LoganDuffy702@users.noreply.github.com>
2019-01-27 01:19:48 -0700

    kjb

---
+++ NightLight/Assets/FanScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanScript : MonoBehaviour
+{
+    public float speed = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(new Vector3(0, speed, 0));
+    }
+}
