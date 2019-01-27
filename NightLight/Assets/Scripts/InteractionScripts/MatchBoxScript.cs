88133fc11cfe5a72ba8d8afbfe4ec8f5da20bdfb
Byreave Luo <654297651@qq.com>
2019-01-26 18:34:08 -0700

    add matchbox prefab

--- NightLight/Assets/Scripts/InteractionScripts/MatchBoxScript.cs
+++ NightLight/Assets/Scripts/InteractionScripts/MatchBoxScript.cs
@@ -2,33 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireplaceLit : MonoBehaviour, Iinteractable
+public class MatchBoxScript : MonoBehaviour, Iinteractable
 {
-    [SerializeField]
-    GameObject FireParticle;
     Dictionary<string, bool> PlayerInventory;
     TextDisplay txtDisplay;
+
     void Start()
     {
         txtDisplay = GameObject.FindGameObjectWithTag("TextDisplay").GetComponent<TextDisplay>();
         //PlayerInventory =  GameObject.FindGameObjectWithTag("GameManager").GetComponent<Inventory>().PlayerItems;
         PlayerInventory = GameObject.Find("gameManager").GetComponent<Inventory>().PlayerItems;
     }
-    public void doPlayerInteraction()
-    {
-        if (PlayerInventory["Day1MatchBox"])
-        {
-            FireParticle.GetComponent<ParticleSystem>().Play();
-            txtDisplay.DisplayText("Warmth, I think that's all I need right now...");
-
-        }
-        else
-            txtDisplay.DisplayText("I need something to light the fireplace...");
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void doPlayerInteraction()
+    {
+        PlayerInventory["Day1MatchBox"] = true;
+        txtDisplay.DisplayText("A Match box. Cool, maybe I can light the fireplace now...");
+        gameObject.SetActive(false);
     }
 }
